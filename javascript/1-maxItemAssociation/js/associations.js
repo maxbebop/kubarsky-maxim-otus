a435c0578d98f3130499bbca2879db07697843fd
Maxim <m.kubarsky@gmail.com>
2022-04-05 22:02:35 +0300

    maxItemAssociation_v2

--- javascript/1-maxItemAssociation/js/associations.js
+++ javascript/1-maxItemAssociation/js/associations.js
@@ -1,18 +1,39 @@
 const associations = ((document)=>{
 
+    class Node {
+        constructor(value){
+            this.value = value;
+            this.weight = 0;
+            this.associations = [];
+            this.addAssociation(value);
+        }
+        
+        addAssociation(value){
+            if(!this.associations.find(item => {return item === value})){
+                this.associations.push(value);
+            }
+            
+        }
+
+        incWeight(){
+            this.weight++;
+        }
+    }
+
     const purchaseHistoryArr_1 = [['a','b','c'],['a','f'],['f','c','g','w'],['w','f'],['f','h'],['d']];
     const purchaseHistoryArr_2 =  [['a', 'b'], ['a', 'c'], ['d', 'e']];
     const purchaseHistoryArr_3 = [['q', 'w', 'a'],['a', 'b'],['a', 'c'],['q', 'e'],['q', 'r'],];
 
     const purchaseHistoryArr = purchaseHistoryArr_1;
+    let associationMap = [];
 
     function maxItemAssociation_v1(buyerHistoryArr){
-        let associations = [];        
+        let associations = [];
         buyerHistoryArr.forEach(purchaseItem => {
             purchaseHistoryArr.forEach(historyItem => {
                 if(historyItem.includes(purchaseItem) && !associations.includes(historyItem)){
                     historyItem.forEach(item => {
-                        if (!associations.includes(item)) {
+                        if (!associations.includes(item) &&  !buyerHistoryArr.includes(item)) {
                             associations.push(item)
                         };
                     });
@@ -23,6 +44,72 @@ const associations = ((document)=>{
         return associations.sort();   
     }
 
+    function maxItemAssociation_v2(buyerHistoryArr){
+        let associationArray = [];        
+        buyerHistoryArr.forEach(purchaseItem => {
+            let mapItem = associationMap.find(item => item.value === purchaseItem);
+            if(mapItem){
+                mapItem.associations.forEach(item => {
+                    if (!associationArray.includes(item)) {
+                        associationArray.push(item);
+                    };
+                });
+            }   
+        });
+        return associationArray.sort(); 
+    }
+
+    function initAssociationMap(purchaseHistoryArr){
+        associationMap = createAssociationMap(purchaseHistoryArr);
+        console.table(associationMap);
+    }
+    function createAssociationMap(purchaseHistoryArr){
+        let weightsArray = createWeightsArray(purchaseHistoryArr);
+        console.table(weightsArray);
+        sordByWeights(weightsArray, purchaseHistoryArr);
+        console.table(purchaseHistoryArr);
+
+        purchaseHistoryArr.forEach(historyItem => {
+            let root = null;  
+            historyItem.forEach (item =>{
+                 if(!root){
+                    root  = weightsArray.find(node => {return node.value === item});
+                 } else {
+                    root.addAssociation(item);
+                 }
+                 
+            });
+        });
+
+        return weightsArray;
+    }
+
+    function sordByWeights(weightsArray, purchaseHistoryArr){
+        purchaseHistoryArr.forEach(historyItem => {
+            historyItem.sort((a,b) => {
+                let aWeight = weightsArray.find(item => {return item.value === a});
+                let bWeight = weightsArray.find(item => {return item.value === b});
+                return bWeight.weight - aWeight.weight;
+            });    
+        }); 
+    }
+    function createWeightsArray(purchaseHistoryArr){
+        let weightArray = [];
+
+        purchaseHistoryArr.forEach(historyItem => {
+            historyItem.forEach(item => {
+                let node = weightArray.find(node => {return node.value === item});
+                if(!node){
+                    node = new Node(item);
+                    weightArray.push(node);
+                }
+                node.incWeight();
+            });    
+        }); 
+       return weightArray.sort((a,b) => {return b.weight - a.weight});
+    }
+
+
 
     function onSelectBuyerHistory(args){
         
@@ -31,7 +118,8 @@ const associations = ((document)=>{
         if(i >= 0) {
             const buyerHistoryArr = purchaseHistoryArr[i];
             const association = maxItemAssociation_v1(buyerHistoryArr);
-            recommendationsEl.innerHTML = `v1: ${association.toString()}`;
+            const association2 = maxItemAssociation_v2(buyerHistoryArr);
+            recommendationsEl.innerHTML = `v1: ${association.toString()} <br/> v2: ${association2.toString()}`;
             
         } else{
             recommendationsEl.textContent = '';
@@ -88,6 +176,7 @@ const associations = ((document)=>{
     function associations(){
         initBuyerHistoryEl();
         initFullHistoryEl();
+        initAssociationMap(purchaseHistoryArr);
     }
     
     return associations;
