555330039f3b629036b3592900365a8cdc0128be
Maxim <m.kubarsky@gmail.com>
2022-04-05 21:49:56 +0300

    homework

--- javascript/1-maxItemAssociation/js/sum.js
+++ javascript/1-maxItemAssociation/js/sum.js
@@ -1,6 +1,6 @@
 const main = ((document)=>{
 
-    const makeAdder = () => {
+    const summ = () => {
         let result = 0;
         return (val) => {
             if(val){
@@ -18,17 +18,17 @@ const main = ((document)=>{
     }
 
     function makeSumm(){
-        const adder = makeAdder();
+        const summator = summ();
         const sumResult = document.getElementById('sumResult');
         sumResult.textContent  = '';
         let strRes = '';
         for(let i = 0; i < 10; i++){
             let val = Math.floor(Math.random() * 11);
             strRes += `${val};`;
-            adder(val);
+            summator(val);
         }
         
-        strRes +=  ` summ: ${adder()};`;
+        strRes +=  ` summ: ${summator()};`;
         sumResult.textContent  =strRes;  
     }
 
