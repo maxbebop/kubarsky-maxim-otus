ed0bc1b647cb472e18214cb6e89411fee29b446f
Maxim <m.kubarsky@gmail.com>
2022-04-17 21:21:29 +0300

    codereview

--- javascript/2-promiseReduce/js/app.js
+++ javascript/2-promiseReduce/js/app.js
@@ -13,45 +13,21 @@ var fn3 = () => new Promise(resolve => {
   setTimeout(() => resolve(3), 1000);
 });
 
-
 function promiseReduce(asyncFunctions, reduce, initialValue) {
 
-  return asyncFunctions.reduce((prevFuncPromise, currentFunc, index) => {
-    return currentFunc().then(currResult => {
-      if (index === 0) {
-        return reduce(initialValue, currResult);
-      }
-      return prevFuncPromise.then(prevResult => {
-        return reduce(prevResult, currResult);
-      });
-    });
-  }, initialValue);
-
-}
-
-function promiseReduce2(asyncFunctions, reduce, initialValue) {
-
   return asyncFunctions.reduce((prevFuncPromise, currentFunc) => {
-    return currentFunc().then(currResult => {
-      return prevFuncPromise.then(prevResult => {
+    return prevFuncPromise.then(currResult => {
+      return currentFunc().then(prevResult => {
         return reduce(prevResult, currResult);
       });
     });
   }, Promise.resolve(initialValue));
 
 }
-console.log('----- v1 -------');
+
 
 promiseReduce([fn1, fn2, fn3], function (memo, value) {
   console.log(`reduce memo: ${memo};  value: ${value}`);
   return memo * value;
-}, 1).then(console.log).then(() => {
-
-  console.log('----- v2 --------');
-  promiseReduce2([fn1, fn2, fn3], function (memo, value) {
-    console.log(`reduce memo: ${memo};  value: ${value}`);
-    return memo * value;
-  }, 1).then(console.log);
-
-});
+}, 1).then(console.log);
 
